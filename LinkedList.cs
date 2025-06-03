186a7107e94eaa32a3f7f58d76647c4f1a0e4163
trendyllama <agog.pretzel-0o@icloud.com>
2025-06-03 22:54:56 +0000

    init commit

---
+++ LinkedList.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet_dsa;
+
+public class LinkedList<T>
+{
+
+    public Node<T>? Head { get; private set; }
+
+    public Node<T>? Tail { get; private set; }
+    public int Count { get; private set; }
+
+}
---
+++ Node.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_dsa;
+
+public class Node<T>(T? value)
+{
+    public T? Value = value;
+    public Node<T>? Next = null;
+    public Node<T>? Previous = null;
+
+}
